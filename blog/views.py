23d34692d1079a224751b21f81adc68a0343d704
Michal22git <michalfiszer15@gmail.com>
2023-09-13 19:39:38 +0200

    3

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+
 def home(request):
-    return HttpResponse("<h1>BLOG HOME</h1>")
+    context = {
+        "posts": posts,
+        "title": "HOME"
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse("<h1>ABOUT PAGE</h1>")
+    return render(request, 'blog/about.html')
